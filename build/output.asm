acb13a940b41727a0160d7a97bff2ff075df0527
Shaun Moate <shaun.moate@gmail.com>
2022-08-10 22:25:57 +0100

    Implemented 'rule90'

--- build/output.asm
+++ build/output.asm
@@ -35,77 +35,371 @@ dump:
 global _start
 _start:
 addr_0:
-    push 69
+    push mem
 addr_1:
-    pop rdi
-    call dump
+    push 49
 addr_2:
-    push 420
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
 addr_3:
-    push 69
-addr_4:
-    pop rax
-addr_5:
-    pop rdi
-    call dump
-addr_6:
-    push 69
-addr_7:
-    push 420
-addr_8:
-    pop rax
-    pop rbx
-    push rbx
-    push rax
-    push rbx
-addr_9:
-    pop rdi
-    call dump
-addr_10:
-    pop rdi
-    call dump
-addr_11:
-    pop rdi
-    call dump
-addr_12:
     push 1
-addr_13:
-    push 2
-addr_14:
+addr_4:
+    pop rbx
+    pop rax
+    mov [rax], bl
+addr_5:
+    push 0
+addr_6:
+addr_7:
+    pop rax
+    push rax
+    push rax
+addr_8:
+    push 49
+addr_9:
+    mov rcx, 0
+    mov rdx, 1
     pop rax
     pop rbx
+    cmp rbx, rax
+    cmovl rcx, rdx
+    push rcx
+addr_10:
+    pop rax
+    test rax, rax
+    jz addr_106
+addr_11:
+    push 0
+addr_12:
+addr_13:
+    pop rax
     push rax
-    push rbx
+    push rax
+addr_14:
+    push 100
 addr_15:
-    pop rdi
-    call dump
+    mov rcx, 0
+    mov rdx, 1
+    pop rax
+    pop rbx
+    cmp rbx, rax
+    cmovl rcx, rdx
+    push rcx
 addr_16:
-    pop rdi
-    call dump
+    pop rax
+    test rax, rax
+    jz addr_44
 addr_17:
-    push 69
+    pop rax
+    push rax
+    push rax
 addr_18:
-    push 420
+    push mem
 addr_19:
     pop rax
     pop rbx
-    push rbx
-    push rax
-    push rbx
+    add rax, rbx
     push rax
 addr_20:
-    pop rdi
-    call dump
+    pop rax
+    xor rbx, rbx
+    mov bl, [rax]
+    push rbx
 addr_21:
-    pop rdi
-    call dump
+    pop rax
+    test rax, rax
+    jz addr_28
 addr_22:
-    pop rdi
-    call dump
+    push mem
 addr_23:
-    pop rdi
-    call dump
+    push 100
 addr_24:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_25:
+    push 42
+addr_26:
+    pop rbx
+    pop rax
+    mov [rax], bl
+addr_27:
+    jmp addr_33
+addr_28:
+    push mem
+addr_29:
+    push 100
+addr_30:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_31:
+    push 32
+addr_32:
+    pop rbx
+    pop rax
+    mov [rax], bl
+addr_33:
+    jmp addr_34
+addr_34:
+    push 1
+addr_35:
+    push mem
+addr_36:
+    push 100
+addr_37:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_38:
+    push 1
+addr_39:
+    push 1
+addr_40:
+    pop rax
+    pop rdi
+    pop rsi
+    pop rdx
+    syscall
+addr_41:
+    push 1
+addr_42:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_43:
+    jmp addr_12
+addr_44:
+    pop rax
+addr_45:
+    push mem
+addr_46:
+    push 100
+addr_47:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_48:
+    push 10
+addr_49:
+    pop rbx
+    pop rax
+    mov [rax], bl
+addr_50:
+    push 1
+addr_51:
+    push mem
+addr_52:
+    push 100
+addr_53:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_54:
+    push 1
+addr_55:
+    push 1
+addr_56:
+    pop rax
+    pop rdi
+    pop rsi
+    pop rdx
+    syscall
+addr_57:
+    push mem
+addr_58:
+    push 0
+addr_59:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_60:
+    pop rax
+    xor rbx, rbx
+    mov bl, [rax]
+    push rbx
+addr_61:
+    push 1
+addr_62:
+    pop rcx
+    pop rax
+    shl rax, cl
+    push rax
+addr_63:
+    push mem
+addr_64:
+    push 1
+addr_65:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_66:
+    pop rax
+    xor rbx, rbx
+    mov bl, [rax]
+    push rbx
+addr_67:
+    pop rax
+    pop rbx
+    or rax, rbx
+    push rax
+addr_68:
+    push 1
+addr_69:
+addr_70:
+    pop rax
+    push rax
+    push rax
+addr_71:
+    push 98
+addr_72:
+    mov rcx, 0
+    mov rdx, 1
+    pop rax
+    pop rbx
+    cmp rbx, rax
+    cmovl rcx, rdx
+    push rcx
+addr_73:
+    pop rax
+    test rax, rax
+    jz addr_101
+addr_74:
+    pop rax
+    pop rbx
+    push rax
+    push rbx
+addr_75:
+    push 1
+addr_76:
+    pop rcx
+    pop rax
+    shl rax, cl
+    push rax
+addr_77:
+    push 7
+addr_78:
+    pop rax
+    pop rbx
+    and rax, rbx
+    push rax
+addr_79:
+    pop rax
+    pop rbx
+    push rbx
+    push rax
+    push rbx
+addr_80:
+    push 1
+addr_81:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_82:
+    push mem
+addr_83:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_84:
+    pop rax
+    xor rbx, rbx
+    mov bl, [rax]
+    push rbx
+addr_85:
+    pop rax
+    pop rbx
+    or rax, rbx
+    push rax
+addr_86:
+    pop rax
+    pop rbx
+    push rbx
+    push rax
+    push rbx
+    push rax
+addr_87:
+    push 90
+addr_88:
+    pop rax
+    pop rbx
+    push rax
+    push rbx
+addr_89:
+    pop rcx
+    pop rax
+    shr rax, cl
+    push rax
+addr_90:
+    push 1
+addr_91:
+    pop rax
+    pop rbx
+    and rax, rbx
+    push rax
+addr_92:
+    pop rax
+    pop rbx
+    push rax
+    push rbx
+addr_93:
+    push mem
+addr_94:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_95:
+    pop rax
+    pop rbx
+    push rax
+    push rbx
+addr_96:
+    pop rbx
+    pop rax
+    mov [rax], bl
+addr_97:
+    pop rax
+    pop rbx
+    push rax
+    push rbx
+addr_98:
+    push 1
+addr_99:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_100:
+    jmp addr_69
+addr_101:
+    pop rax
+addr_102:
+    pop rax
+addr_103:
+    push 1
+addr_104:
+    pop rax
+    pop rbx
+    add rax, rbx
+    push rax
+addr_105:
+    jmp addr_6
+addr_106:
+    pop rax
+addr_107:
     mov rax, 60
     mov rdi, 0
     syscall
